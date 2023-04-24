2bfda225de44e3c73816152b66b2afff2b3acce7
akira <zgdanny@gamail.com>
2023-04-24 14:04:34 +0800

    test

---
+++ Mario/source/setup.py
@@ -0,0 +1,12 @@
+# 编写者：cwt
+# 时间：2022/7/4 21:25
+# 启动代码
+import sys
+import os
+
+GRAPHICS = None
+SOUND = None
+MUSIC = None
+
+# 获取启动脚本所在的目录
+script_dir = os.path.dirname(os.path.abspath(sys.argv[0]))
